d9520025a640ad1e5a393ef91c5390226f0ad48f
leand12 <leandrosilva12@ua.pt>
2021-11-29 10:24:43 +0000

    css show team member info

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,7 +1,25 @@
 import "styles/Content.css";
 import eu from 'assets/eu.jpg';
+import { useEffect } from "react";
+import * as d3 from "d3";
 
 export default function Content() {
+
+    function color(d, i) {
+        if (i % 3 == 0)
+            return "rgba(0, 94, 124, 0.6)"
+        if (i % 3 == 1)
+            return "rgba(0, 138, 82, 0.6)"
+        return "rgba(0, 118, 106, 0.6)"
+    }
+
+    useEffect(() => {
+        d3.selectAll('.hexagon h1')
+            .style("background-color", color);
+        d3.selectAll('.hexagon p')
+            .style("background-color", color);
+    }, [])
+
     return (
         <>
             <h2>Equipa</h2>
@@ -13,11 +31,20 @@ export default function Content() {
                 <li>Exploração da possibilidade de um novo Sunset.</li>
             </ul>
             <div className="hexa-row">
-                <div className="hexagon" style={{ backgroundImage: `url(${eu})` }}></div>
-                <div className="hexagon" style={{ backgroundImage: `url(${eu})` }}></div>
+                <div className="hexagon" style={{ backgroundImage: `url(${eu})` }}>
+                    <h1>Leandro Silva</h1>
+                    <p>1º ano MEI</p>
+                </div>
+                <div className="hexagon" style={{ backgroundImage: `url(${eu})` }}>
+                    <h1>Leandro Silva</h1>
+                    <p>1º ano MEI</p>
+                </div>
             </div>
             <div className="hexa-row">
-                <div className="hexagon" style={{ backgroundImage: `url(${eu})` }}></div>
+                <div className="hexagon" style={{ backgroundImage: `url(${eu})` }}>
+                    <h1>Leandro Silva</h1>
+                    <p>1º ano MEI</p>
+                </div>
             </div>
         </>
     );
