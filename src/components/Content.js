7aaf7b46756e3975f7aa2414168581cf0b8dc770
leand12 <leandrosilva12@ua.pt>
2021-12-01 22:49:12 +0000

    tabs

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,51 +1,119 @@
 import "styles/Content.css";
-import eu from 'assets/eu.jpg';
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import * as d3 from "d3";
+import { data } from './data';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import { styled } from '@mui/material/styles';
+
+
+const color = (i) => {
+    if (i % 3 == 0)
+        return "0, 94, 124";
+    if (i % 3 == 1)
+        return "0, 138, 82";
+    return "0, 118, 106";
+}
+
+const CustomTabs = styled((props) => (
+    <Tabs
+        {...props}
+        TabIndicatorProps={{ children: <span className="MuiTabs-indicatorSpan" /> }}
+    />
+))({
+    '& .MuiTabs-indicator': {
+        display: 'flex',
+        justifyContent: 'center',
+        backgroundColor: 'transparent',
+    },
+    '& .MuiTabs-indicatorSpan': {
+        maxWidth: 40,
+        width: '100%',
+    },
+});
+
+const CustomTab = styled((props) => <Tab disableRipple {...props} />)(
+    ({ theme }) => ({
+        textTransform: 'uppercase',
+        fontFamily: 'Poppins',
+        fontWeight: theme.typography.fontWeightBold,
+        fontSize: theme.typography.pxToRem(15),
+        marginRight: theme.spacing(1),
+        color: 'rgba(0, 0, 0, 0.3)',
+        '&.Mui-selected:nth-of-type(3n)': {
+            color: `rgb(${color(0)})`,
+        },
+        '&.Mui-selected:nth-of-type(3n+1)': {
+            color: `rgb(${color(1)})`,
+        },
+        '&.Mui-selected:nth-of-type(3n+2)': {
+            color: `rgb(${color(2)})`,
+        },
+        '&.Mui-focusVisible': {
+            backgroundColor: 'rgba(100, 95, 228, 0.32)',
+        },
+    }),
+);
 
 export default function Content() {
 
-    function color(d, i) {
-        if (i % 3 == 0)
-            return "rgba(0, 94, 124, 0.6)"
-        if (i % 3 == 1)
-            return "rgba(0, 138, 82, 0.6)"
-        return "rgba(0, 118, 106, 0.6)"
-    }
+    const [value, setValue] = useState(0)
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
 
     useEffect(() => {
         d3.selectAll('.hexagon h1')
-            .style("background-color", color);
+            .style("background-color", (d, i) => `rgba(${color(i)}, 0.6)`);
         d3.selectAll('.hexagon p')
-            .style("background-color", color);
-    }, [])
+            .style("background-color", (d, i) => `rgba(${color(i)}, 0.6)`);
+        d3.select('.MuiTabs-indicatorSpan')
+            .style("background-color", `rgb(${color(value + 1)})`);
+
+    }, [value])
 
     return (
         <div id="Content">
-            <div className="content-wrapper">
-                <h2>Equipa</h2>
-                <ul>
-                    <li>Organização de um convívio temático.</li>
-                    <li>Organização de um jantar de gala.</li>
-                    <li>Participação no Enterro.</li>
-                    <li>Organização do Rally das Tascas.</li>
-                    <li>Exploração da possibilidade de um novo Sunset.</li>
-                </ul>
+            {/* <Box sx={{ maxWidth: 480, bgcolor: 'background.paper' }}> */}
+            <CustomTabs
+                value={value}
+                onChange={handleChange}
+                variant="scrollable"
+                scrollButtons
+                allowScrollButtonsMobile
+                aria-label="scrollable force tabs example"
+                sx={{ margin: 'auto' }}
+            >
+                {
+                    data.map(({ section }, index) =>
+                        <CustomTab key={index} label={section} />
+                    )
+                }
+            </CustomTabs>
+
+            <div className="tab-content">
+
+                <div className="content-wrapper">
+                    {
+                        data[value].content
+                    }
+                </div>
+                <div className="hexa-wrapper">
+                    {
+                        data[value].members.map((member, index) => (
+                            <div key={index} className="hexagon"
+                                style={data[value].members.length == 1 ?
+                                    { backgroundImage: `url(${member.image})`, marginLeft: "25%" } : 
+                                    { backgroundImage: `url(${member.image})` }}
+                            >
+                                <h1>{member.name}</h1>
+                                <p>{member.year}</p>
+                            </div>
+                        ))
+                    }
+                </div>
             </div>
-            <div className="hexa-wrapper">
-                <div className="hexagon" style={{ backgroundImage: `url(${eu})` }}>
-                    <h1>Leandro Silva</h1>
-                    <p>1º ano MEI</p>
-                </div>
-                <div className="hexagon" style={{ backgroundImage: `url(${eu})` }}>
-                    <h1>Leandro Silva</h1>
-                    <p>1º ano MEI</p>
-                </div>
-                <div className="hexagon" style={{ backgroundImage: `url(${eu})` }}>
-                    <h1>Leandro Silva</h1>
-                    <p>1º ano MEI</p>
-                </div>
-            </div>
-        </div>
+        </div >
     );
 }
--- src/components/data.js
+++ src/components/data.js
@@ -1,14 +1,136 @@
+import eu from 'assets/eu.jpg';
+
 export const data = [
     {
         section: "Coordenador + RF",
         content: (
-            <div></div>
-        )
+            <></>
+        ),
+        members: [
+            {
+                name: "Pedro Figueiredo",
+                year: "3º ano LEI",
+                image: eu,
+            },
+            {
+                name: "Artur Correia",
+                year: "2º ano LEI",
+                image: eu,
+            },
+        ],
     },
     {
-        section: "Coordenador + RF",
+        section: "Académica",
         content: (
-            <div></div>
-        )
+            <ul>
+                <li>Organização de um convívio temático.</li>
+                <li>Organização de um jantar de gala.</li>
+                <li>Participação no Enterro.</li>
+                <li>Organização do Rally das Tascas.</li>
+                <li>Exploração da possibilidade de um novo Sunset.</li>
+            </ul>
+        ),
+        members: [
+            {
+                name: "Fábio Martins",
+                year: "3º ano LEI",
+                image: eu,
+            },
+            {
+                name: "Daniel Ferreira",
+                year: "2º ano LEI",
+                image: eu,
+            },
+        ],
+    },
+    {
+        section: "Administrativa",
+        content: (
+            <></>
+        ),
+        members: [
+            {
+                name: "Leandro Silva",
+                year: "1º ano MEI",
+                image: eu,
+            },
+        ],
+    },
+    {
+        section: "Comunicação e Imagem",
+        content: (
+            <></>
+        ),
+        members: [
+            {
+                name: "Yanis Faquir",
+                year: "3º ano LEI",
+                image: eu,
+            },
+            {
+                name: "Matilde Teixeira",
+                year: "1º ano LEI",
+                image: eu,
+            },
+        ],
+    },
+    {
+        section: "Desportiva",
+        content: (
+            <></>
+        ),
+        members: [
+            {
+                name: "Paulo Pereira",
+                year: "3º ano LEI",
+                image: eu,
+            },
+            {
+                name: "Hugo Correia",
+                year: "1º ano LEI",
+                image: eu,
+            },
+        ],
+    },
+    {
+        section: "Pedagógica e Social",
+        content: (
+            <></>
+        ),
+        members: [
+            {
+                name: "Miguel Ferreira",
+                year: "3º ano LEI",
+                image: eu,
+            },
+            {
+                name: "Vicente Barros",
+                year: "2º ano LEI",
+                image: eu,
+            },
+        ],
+    },
+    {
+        section: "Mesa da RGM",
+        content: (
+            <></>
+        ),
+        members: [
+            {
+                name: "Renato Dias",
+                year: "3º ano LEI",
+                image: eu,
+            },
+            {
+                name: "Tiago Gomes",
+                year: "1º ano LEI",
+                image: eu,
+            },
+            {
+                name: "Rafaela Abrunhosa",
+                year: "1º ano LEI",
+                image: eu,
+            },
+        ],
     }
 ]
